78baa8e565884fc2b1302428b3fc70bf1bd4bcef
benjiekern <38410280+benjiekern@users.noreply.github.com>
2022-03-21 13:20:25 -0700

    fixed error (random backslashes were placed in front of routes)

--- CogniClothUI/src/app/app-routing.module.ts
+++ CogniClothUI/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './compoenent/account/account.component';
-import { CartComponent } from './compoenent/cart/cart.component';
-import { ProductsComponent } from './compoenent/products/products.component';
+import { AppComponent } from './app.component';
+import { AccountComponent } from './component/account/account.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
 
 
 const routes: Routes = [
-  {path:'/cart',component:CartComponent},
-  {path:'/account',component:AccountComponent},
-  {path:'/products',component:ProductsComponent}
+  {path:'cart',component:CartComponent},
+  {path:'account',component:AccountComponent},
+  {path:'products',component:ProductsComponent}
 ];
 
 @NgModule({
